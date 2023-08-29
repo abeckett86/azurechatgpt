a34c412d78b73c5e00822c8d5f0a1bd4c1ae8894
Jon Harvey <jon.harvey@cherrythinking.com>
2023-08-29 15:24:50 +0100

    Branding amends

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#A16A38",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
